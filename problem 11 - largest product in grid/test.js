3c02e0b42318d46ab15efd6e282554096b122f57
unknown <elii236@gmail.com>
2020-12-11 17:47:56 +0200

    problem 11 solved!

---
+++ problem 11 - largest product in grid/test.js
@@ -0,0 +1,25 @@
+
+let i, j;
+for(i = 0, j = 1; i < 10; i++, j++) {
+    console.log("i =  " + i);
+    console.log("j = " + j);
+}
+
+let testArray = [
+    [1, 2],
+    [3, 4],
+    [5, 6],
+    [7, 8],
+    [9, 10]
+]
+
+function letsSee(grid) {
+    const revGrid = grid.slice(0, 3).reverse();
+    
+    return 1;
+}
+
+
+letsSee(testArray);
+
+console.log(testArray);
